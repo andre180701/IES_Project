ee8616b917383d63c59562d91a7cb46ce5158693
luisccmartins <luisccmartins88@gmail.com>
2021-12-13 18:35:59 +0000

    Maven Project

---
+++ projFastTravel/ProjService/FastTravelService/src/main/java/com/FastTravel/FastTravelService/FastTravelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.FastTravel.FastTravelService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTravelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastTravelServiceApplication.class, args);
+	}
+
+}
---
+++ projFastTravel/ProjService/FastTravelService/src/test/java/com/FastTravel/FastTravelService/FastTravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FastTravel.FastTravelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTravelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
