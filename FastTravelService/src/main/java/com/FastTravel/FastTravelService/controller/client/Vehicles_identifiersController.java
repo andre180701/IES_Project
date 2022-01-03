b63a1c81aefadd03c24375102384ffb3592f79ec
eva-pomposo <evabartolomeu@ua.pt>
2022-01-03 22:49:01 +0000

    fix html

--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/Vehicles_identifiersController.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/Vehicles_identifiersController.java
@@ -1,5 +1,15 @@
 package com.FastTravel.FastTravelService.controller.client;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class Vehicles_identifiersController {
+
+    @GetMapping("/vehicles_identifiers")
+    public String getAllVehicles_identifiers( Model model) {
+		return "client/vehicles_identifiers.html";
+    }  
     
 }
