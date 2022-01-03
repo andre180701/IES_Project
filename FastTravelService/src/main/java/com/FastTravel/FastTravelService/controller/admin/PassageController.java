731b9cc686634d48e7c84f68221def06249fa857
eva-pomposo <evabartolomeu@ua.pt>
2022-01-03 21:49:51 +0000

    create folder admin in folder controller

---
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/admin/PassageController.java
@@ -0,0 +1,38 @@
+package com.FastTravel.FastTravelService.controller.admin;
+
+
+import com.FastTravel.FastTravelService.model.Passage;
+import com.FastTravel.FastTravelService.repository.PassageRepository;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
+import java.util.*;
+
+@Controller
+//@EnableJpaRepositories(basePackageClasses = {PassageRepository.class})
+public class PassageController {
+    @Autowired
+    private PassageRepository passageRepository;
+
+    //@CrossOrigin(origins = "http://localhost:8000/")
+    @GetMapping("/passages")
+    public String getAllPassages( Model model) {
+        List<Passage> passages = passageRepository.findAll();
+
+        
+        
+		model.addAttribute("passages", passages);
+		return "admin/passages";
+    }   
+
+    @PostMapping("/passages")
+    public  @Valid Passage createPassage(@Valid @RequestBody Passage passage){
+        return passageRepository.save(passage);
+    }
+
+}
---
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/Vehicles_identifiersController.java
@@ -0,0 +1,5 @@
+package com.FastTravel.FastTravelService.controller.client;
+
+public class Vehicles_identifiersController {
+    
+}
