ed9373a8edc3f5bcbd64d9c3e04482f8d001ff00
luisccmartins <luisccmartins88@gmail.com>
2022-01-06 20:15:40 +0000

    update LoginController

--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/repository/ClientRepository.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/repository/ClientRepository.java
@@ -2,9 +2,11 @@ package com.FastTravel.FastTravelService.repository;
 
 import com.FastTravel.FastTravelService.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    
+    //public Client findByEmail(@Param("email") String email);
+
 }
