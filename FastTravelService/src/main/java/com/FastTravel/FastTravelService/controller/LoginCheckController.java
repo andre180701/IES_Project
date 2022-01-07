77e779fdc9a9a2efacd6d2b379e1744387967662
eva-pomposo <evabartolomeu@ua.pt>
2022-01-07 21:21:34 +0000

    try implementation login again

--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/LoginCheckController.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/LoginCheckController.java
@@ -4,10 +4,13 @@ import com.FastTravel.FastTravelService.exception.*;
 import com.FastTravel.FastTravelService.model.Client;
 import com.FastTravel.FastTravelService.service.ClientService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.json.simple.JSONObject;  
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;  
 
 @RestController
 public class LoginCheckController {
@@ -15,15 +18,29 @@ public class LoginCheckController {
     @Autowired
     private ClientService clientService;
 
-    @CrossOrigin(origins ="http://127.0.0.1:6868/login")
-    @GetMapping("/login/check")
-    public String getLogin(String email, String password) throws ResourceNotFoundException {
+    @PostMapping("/login/check")
+    public JSONObject getLogin(String email, String password) throws ResourceNotFoundException {
+        JSONParser parser = new JSONParser();  
+        JSONObject json = null;
         Client client = clientService.getClientByEmail(email);      
 
         if(client != null && client.getPassword().equals(password)) {
             System.out.println("{\"state\": true, \"firstName\": \"" + client.getFirst_name() +"\", \"lastName\": \"" + client.getLast_name() + "\" }");
-            return "{\"state\": true, \"firstName\": \"" + client.getFirst_name() +"\", \"lastName\": \"" + client.getLast_name() + "\"}";
+            try {
+                json = (JSONObject) parser.parse("{\"state\": true, \"firstName\": \"" + client.getFirst_name() +"\", \"lastName\": \"" + client.getLast_name() + "\"}");
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return json;
         }
-        return "{\"state\": false, \"error\": \"Email and password incorrect!\"}";
+        try {
+            json = (JSONObject) parser.parse("{\"state\": false, \"error\": \"Email and password incorrect!\"}");
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return json;
     }
 }
