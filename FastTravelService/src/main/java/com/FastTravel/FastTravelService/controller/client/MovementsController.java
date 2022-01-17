dd0cb0ec9392ec8cb01bbefa453ba4a92e3cebc2
MartaFradique <martafradique@ua.pt>
2022-01-17 00:48:54 +0000

    price already working

--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/MovementsController.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/MovementsController.java
@@ -65,7 +65,7 @@ public class MovementsController {
 
   @PostMapping("/client/filter")
   public String filterMovements(@ModelAttribute FilterForms filterForms, Model model) {
-    System.out.println("INICIO");
+  
     HttpSession session = httpSessionFactory.getObject();
     String email = (String) session.getAttribute("email");
 
@@ -88,7 +88,6 @@ public class MovementsController {
       }
     }
     
-    System.out.println(passages_client2.size());
     if(filterForms.getRegistration() != ""){
       for(Passage pc : passages_client){
         if(!pc.getIdentifier().getRegistration().equals(filterForms.getRegistration())){
@@ -99,8 +98,6 @@ public class MovementsController {
 
     if(filterForms.getIdentifier() != ""){
       for(Passage pc : passages_client){
-        System.out.println(filterForms.getIdentifier());
-        System.out.println(pc.getIdentifier().getId());
         if(pc.getIdentifier().getId() != (Long.parseLong(String.valueOf(filterForms.getIdentifier())))){
           if(passages_client2.contains(pc)){
             passages_client2.remove(pc);
@@ -124,7 +121,7 @@ public class MovementsController {
     if(filterForms.getHour() != ""){
       for(Passage pc : passages_client){
         if(!pc.getTime().equals(Time.valueOf(filterForms.getHour()))){
-          System.out.println(pc.getTime());
+         
           if(passages_client2.contains(pc)){
             passages_client2.remove(pc);
             
@@ -136,8 +133,6 @@ public class MovementsController {
     }
     if(filterForms.getScutslatitude() != ""){
       for(Passage pc : passages_client){
-        System.out.println(filterForms.getScutslatitude());
-        System.out.println(pc.getScut().getLatitude());
         if(pc.getScut().getLatitude() != Double.parseDouble(filterForms.getScutslatitude()) ){
           
           
@@ -152,10 +147,7 @@ public class MovementsController {
     }
     if(filterForms.getScutslongitude() != ""){
       for(Passage pc : passages_client){
-        System.out.println(filterForms.getScutslongitude());
-        System.out.println(pc.getScut().getLongitude());
         if(pc.getScut().getLongitude() != Double.parseDouble(filterForms.getScutslongitude()) ){
-          
           
           if(passages_client2.contains(pc)){
             passages_client2.remove(pc); 
@@ -168,8 +160,6 @@ public class MovementsController {
     }
     if(filterForms.getScutsdescription() != ""){
       for(Passage pc : passages_client){
-        System.out.println(filterForms.getScutsdescription());
-        System.out.println(pc.getScut().getDescription());
         if(!pc.getScut().getDescription().equals(filterForms.getScutsdescription()) ){
           if(passages_client2.contains(pc)){
             passages_client2.remove(pc); 
@@ -179,8 +169,6 @@ public class MovementsController {
     }
     if(filterForms.getPaymentstate() != ""){
       for(Passage pc : passages_client){
-        System.out.println(filterForms.getPaymentstate());
-        System.out.println(pc.getScut().getDescription());
         if(!pc.getPaymentState().toString().equals(filterForms.getPaymentstate())){
           if(passages_client2.contains(pc)){
             passages_client2.remove(pc); 
@@ -188,59 +176,41 @@ public class MovementsController {
         }
       } 
     }
+
+
+    if(filterForms.getPrice() != ""){
+
+      for(Passage pc : passages_client){
+
+        int classe = pc.getIdentifier().getClasse();
+        double priceClasse = pc.getScut().getPrice5();
+        switch(classe) {
+          case 1:
+            priceClasse = pc.getScut().getPrice1();
+            break;
+          case 2:
+            priceClasse = pc.getScut().getPrice2();
+            break;
+          case 3:
+            priceClasse = pc.getScut().getPrice3();
+            break;
+          case 4:
+            priceClasse = pc.getScut().getPrice4();
+            break;
+          case 5:
+            priceClasse = pc.getScut().getPrice5();
+            break;
+        }
+       
+        if(priceClasse != Double.parseDouble(filterForms.getPrice())){
+          if(passages_client2.contains(pc)){
+            passages_client2.remove(pc); 
+          }
+        }
+      } 
+    }
+
     
-    
-
- 
-    
-
-
-
-    // for(Passage pc : passages_client){
-    //   // int classe = pc.getIdentifier().getClasse();
-    //   //  double priceClasse;
-    //   //  switch(classe) {
-    //   //    case 1:
-    //   //      priceClasse = pc.getScut().getPrice1();
-    //   //      break;
-    //   //    case 2:
-    //   //      priceClasse = pc.getScut().getPrice2();
-    //   //      break;
-    //   //    case 3:
-    //   //      priceClasse = pc.getScut().getPrice2();
-    //   //      break;
-    //   //    case 4:
-    //   //      priceClasse = pc.getScut().getPrice2();
-    //   //      break;
-    //   //    case 5:
-    //   //      priceClasse = pc.getScut().getPrice2();
-    //   //      break;
-    //   //    default:
-    //   //      // code block
-    //   //  }
-    //   System.out.println("matricula inserida");
-    //   System.out.println(filterForms.getRegistration());
-    //   System.out.println("matriculas da geraçao de dados");
-    //   System.out.println(pc.getIdentifier().getRegistration());
-
-                                                                               
-    //   if(pc.getIdentifier().getRegistration().equals(filterForms.getRegistration()) && //FILTER REGISTRATION  
-    //   //pc.getIdentifier().getId().equals(Long.parseLong(filterForms.getIdentifier())) && 
-    //   pc.getDate().equals(Date.valueOf(filterForms.getDate())) && 
-    //   pc.getTime().equals(Time.valueOf(filterForms.getHour())) &&
-    //   //pc.getScut().getLatitude().equals(Double.parseDouble(filterForms.getScutslatitude())) &&
-    //   //pc.getScut().getLongitude() == Double.parseDouble(filterForms.getScutslongitude()) &&
-    //   pc.getScut().getDescription().equals(filterForms.getScutsdescription()) 
-    //   //priceClasse.equals(Double.parseDouble(filterForms.getPrice()))
-    //   //pc.getPaymentState().equals(filterForms.getPaymentstate())
-    //   ){
-    //     filter_passages.add(pc);  
-    //   }
-
-
-    // }
-    System.out.println("Tamanho lista final");
-    System.out.println(passages_client2.size());
 
     model.addAttribute("passages", passages_client2);
     return "client/movements";
