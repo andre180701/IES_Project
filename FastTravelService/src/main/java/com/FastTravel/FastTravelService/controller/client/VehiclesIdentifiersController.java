2d8bb1aaba7264ee23200e70dff883ec0fe11f24
MartaFradique <martafradique@ua.pt>
2022-01-25 00:34:46 +0000

    filters working

--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/VehiclesIdentifiersController.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/client/VehiclesIdentifiersController.java
@@ -1,8 +1,12 @@
 package com.FastTravel.FastTravelService.controller.client;
+import com.FastTravel.FastTravelService.inputsForms.FilterIdentifiers;
+
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import org.springframework.beans.factory.ObjectFactory;
 
@@ -13,6 +17,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.FastTravel.FastTravelService.service.ClientService;
 import com.FastTravel.FastTravelService.model.Client;
+import com.FastTravel.FastTravelService.model.StateIdentifier;
+import com.FastTravel.FastTravelService.model.PaymentState;
 import com.FastTravel.FastTravelService.model.Identifier;
 import com.FastTravel.FastTravelService.controller.IdentifierController;
 
@@ -22,6 +28,11 @@ public class VehiclesIdentifiersController {
 
   @Autowired
   private ClientService clientService;
+
+  @ModelAttribute("filterForm")
+  public FilterIdentifiers getGreetingObject() {
+    return new FilterIdentifiers();
+  }
 
   @Autowired
   ObjectFactory<HttpSession> httpSessionFactory;
@@ -54,4 +65,81 @@ public class VehiclesIdentifiersController {
       model.addAttribute("identifiers", identifiersClient);
 		  return "client/vehiclesIdentifiers";
     }    
+
+  @PostMapping("/client/filteridentifier")
+  public String filterMovements(@ModelAttribute FilterIdentifiers filterIdentifiers, Model model) {
+    HttpSession session = httpSessionFactory.getObject();
+
+    model.addAttribute("firstName", session.getAttribute("firstName"));
+    model.addAttribute("lastName", session.getAttribute("lastName"));
+    model.addAttribute("email", session.getAttribute("email"));
+
+    String email = (String) session.getAttribute("email");
+    Client client = clientService.getClientByEmail(email);
+
+    List<Identifier> identifiersClient = new ArrayList<Identifier>();
+    List<Identifier> identifiersClient2 = new ArrayList<Identifier>();
+    List<Identifier> identifiers = identifierController.findAllIdentifiers();
+
+    for(Identifier val : identifiers){
+      if(client.getId() == val.getClient().getId()){
+        identifiersClient.add(val);
+        identifiersClient2.add(val);
+      }
+    }
+
+    if(filterIdentifiers.getRegistration().strip() != ""){
+      for(Identifier pc : identifiersClient){
+        if(!pc.getRegistration().equals(filterIdentifiers.getRegistration().strip())){
+          identifiersClient2.remove(pc); 
+        }
+      }
+    }
+
+
+
+    if(filterIdentifiers.getIdentifier().strip() != ""){
+      for(Identifier pc : identifiersClient){
+        if(pc.getId() != (Integer.parseInt(String.valueOf(filterIdentifiers.getIdentifier().strip()))) && identifiersClient2.contains(pc)){
+            identifiersClient2.remove(pc);
+        }
+      }
+    }
+
+    if(filterIdentifiers.getClasse().strip() != ""){
+      for(Identifier pc : identifiersClient){
+        if(pc.getClasse() != (Integer.parseInt(String.valueOf(filterIdentifiers.getClasse().strip()))) && identifiersClient2.contains(pc)){
+            identifiersClient2.remove(pc);
+        }
+      }
+    }
+    System.out.println("INICIO");
+    if(filterIdentifiers.getMeansPayment().strip() != ""){
+      System.out.println("Entrou no payment");
+      System.out.println("formulario");
+      System.out.println(filterIdentifiers.getMeansPayment());
+      System.out.println("valor da bd");
+      
+      for(Identifier pc : identifiersClient){
+        System.out.println("valor da bd");
+        System.out.println(pc.getCreditCard().getId());
+        if(pc.getCreditCard().getNumber() != (Long.parseLong(String.valueOf(filterIdentifiers.getMeansPayment().strip()))) && identifiersClient2.contains(pc)){
+            identifiersClient2.remove(pc);
+        }
+      }
+    }
+
+    if(filterIdentifiers.getState().strip() != ""){
+      for(Identifier pc : identifiersClient){
+        if(pc.getState() != StateIdentifier.valueOf(String.valueOf(filterIdentifiers.getState().strip())) && identifiersClient2.contains(pc)){
+            identifiersClient2.remove(pc);
+        }
+      }
+    }
+
+
+
+    model.addAttribute("identifiers", identifiersClient2);
+    return "client/vehiclesIdentifiers";
+  }
 }
---
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/inputsForms/FilterIdentifiers.java
@@ -0,0 +1,52 @@
+package com.FastTravel.FastTravelService.inputsForms;
+
+public class FilterIdentifiers {
+    private String registration;
+    private String identifier;
+    private String classe;
+    private String meansPayment;
+    private String state;
+
+
+    public String getRegistration() {
+        return this.registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getClasse() {
+        return this.classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getMeansPayment() {
+        return this.meansPayment;
+    }
+
+    public void setMeansPayment(String meansPayment) {
+        this.meansPayment = meansPayment;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+}
