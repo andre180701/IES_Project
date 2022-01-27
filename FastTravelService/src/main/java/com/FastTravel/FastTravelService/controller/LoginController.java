ee80575f8487811ccd904ed6eaa777712c087558
eva-pomposo <evabartolomeu@ua.pt>
2022-01-27 00:45:25 +0000

    try implement another grah

--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/LoginController.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/LoginController.java
@@ -9,19 +9,17 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest; 
 import java.security.NoSuchAlgorithmException;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
 import com.FastTravel.FastTravelService.inputsForms.InputLogin;
 import com.FastTravel.FastTravelService.model.Admin;
 import com.FastTravel.FastTravelService.model.Client;
 import com.FastTravel.FastTravelService.model.Passage;
 import com.FastTravel.FastTravelService.service.AdminService;
 import com.FastTravel.FastTravelService.service.ClientService;
-import com.FastTravel.FastTravelService.controller.PassageController;
-
 import org.springframework.beans.factory.ObjectFactory;
 import javax.servlet.http.HttpSession;  
 
@@ -98,6 +96,7 @@ public class LoginController {
     Admin admin = adminService.getAdminByEmail(email);
     
     if(admin != null && admin.getPassword().equals(password)) {
+      System.out.println("ANtes de tudo");
       session.setAttribute("email", email);
       session.setAttribute("firstName", admin.getFirst_name());
       session.setAttribute("lastName", admin.getLast_name());
@@ -105,8 +104,9 @@ public class LoginController {
       model.addAttribute("lastName", admin.getLast_name());
       model.addAttribute("email", email);
 
-
+      System.out.println("ANTES DE IR VUSCAR COISAS");
       HashMap<String, Integer> hm = new HashMap<String, Integer>(); 
+      HashMap<Date, Double> hmProfit  = new HashMap<Date, Double>(); 
       List<Passage> passages = passageController.findAllPassages();
       for(Passage p : passages) {
         if(hm.containsKey(p.getScut().getDescription())){
@@ -116,15 +116,48 @@ public class LoginController {
         else{
           hm.put(p.getScut().getDescription(), 1);
         }
+
+        if(hmProfit.containsKey(p.getDate())){
+          hmProfit.put(p.getDate(), 0.0);
+        }
+
+        switch (p.getIdentifier().getClasse()) {
+          case 1:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice1());
+            break;
+          case 2:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice2());
+            break;
+          case 3:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice3());
+            break;
+          case 4:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice4());
+            break;
+          default:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice5());
+            break;
+        }
+        
+        System.out.println("CHave: " + String.valueOf(p.getDate()) + ", Tipo:" + String.valueOf(p.getDate()).getClass().getName());
+        System.out.println("Value: " + String.valueOf(hmProfit.get(p.getDate())) + ", Tipo:" + String.valueOf(hmProfit.get(p.getDate())).getClass().getName());
       }
 
 
       Map<String, Integer> graphData = new TreeMap<>();
+      Map<String, String> graphDataProfit = new TreeMap<>();
       for(String i : hm.keySet()) {
         graphData.put(i, hm.get(i));
       }
+      System.out.println("New grah");
+      for(Date i : hmProfit.keySet()) {
+        graphDataProfit.put(String.valueOf(i), String.valueOf(hmProfit.get(i)));
+        System.out.println("CHave: " + String.valueOf(i) + ", Tipo:" + String.valueOf(i).getClass().getName());
+        System.out.println("Value: " + String.valueOf(hmProfit.get(i)) + ", Tipo:" + String.valueOf(hmProfit.get(i)).getClass().getName());
+      }
       
       model.addAttribute("chartData", graphData);
+      model.addAttribute("chartDataProfit", graphDataProfit);
       
       return "admin/dashboard";
     }
--- FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/admin/DashboardController.java
+++ FastTravelService/src/main/java/com/FastTravel/FastTravelService/controller/admin/DashboardController.java
@@ -1,22 +1,18 @@
 package com.FastTravel.FastTravelService.controller.admin;
 
 import org.springframework.ui.Model;
-import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.ObjectFactory;
 import com.FastTravel.FastTravelService.model.Passage;
-import com.FastTravel.FastTravelService.repository.PassageRepository;
 import com.FastTravel.FastTravelService.controller.PassageController;
 import javax.servlet.http.HttpSession;  
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import javax.validation.Valid;
- 
-
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class DashboardController {
@@ -37,6 +33,7 @@ public class DashboardController {
 
 
       HashMap<String, Integer> hm = new HashMap<String, Integer>(); 
+      HashMap<Date, Double> hmProfit  = new HashMap<Date, Double>(); 
       List<Passage> passages = passageController.findAllPassages();
       for(Passage p : passages) {
         if(hm.containsKey(p.getScut().getDescription())){
@@ -46,15 +43,45 @@ public class DashboardController {
         else{
           hm.put(p.getScut().getDescription(), 1);
         }
+
+        if(hmProfit.containsKey(p.getDate())){
+          hmProfit.put(p.getDate(), 0.0);
+        }
+
+        switch (p.getIdentifier().getClasse()) {
+          case 1:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice1());
+            break;
+          case 2:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice2());
+            break;
+          case 3:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice3());
+            break;
+          case 4:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice4());
+            break;
+          default:
+            hmProfit.put(p.getDate(), hmProfit.get(p.getDate()) + p.getScut().getPrice5());
+            break;
+        }
       }
 
 
       Map<String, Integer> graphData = new TreeMap<>();
+      Map<String, String> graphDataProfit = new TreeMap<>();
       for(String i : hm.keySet()) {
         graphData.put(i, hm.get(i));
       }
+      System.out.println("New grah");
+      for(Date i : hmProfit.keySet()) {
+        graphDataProfit.put(String.valueOf(i), String.valueOf(hmProfit.get(i)));
+        System.out.println("CHave: " + String.valueOf(i) + ", Tipo:" + String.valueOf(i).getClass().getName());
+        System.out.println("Value: " + String.valueOf(hmProfit.get(i)) + ", Tipo:" + String.valueOf(hmProfit.get(i)).getClass().getName());
+      }
       
       model.addAttribute("chartData", graphData);
+      model.addAttribute("chartDataProfit", graphDataProfit);
 
 		return "admin/dashboard";
     }
